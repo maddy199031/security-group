b7787f57b17149129eb8f9fddb3439a6ca1ce4db
maddy199031 <59460532+maddy199031@users.noreply.github.com>
2021-09-23 19:01:46 +0530

    Update output.tf

--- src/output.tf
+++ src/output.tf
@@ -1,9 +1,10 @@
-output "security_group_id" {
-  value = aws_security_group.sg.id
-}
-output "sg_egress_rules_id" {
-  value = aws_security_group_rule.egress_rules.*.id
-}
-output "sg_ingress_rules_id" {
-  value = aws_security_group_rule.ingress_rules.*.id
+
+
+output "v_1_0_7" {
+  
+    value = {
+        security_group_id = aws_security_group.sg.id
+        sg_egress_rules_id= aws_security_group_rule.egress_rules.*.id
+        sg_ingress_rules_id = aws_security_group_rule.ingress_rules.*.id
+    }
 }
